df3f80b93be5f98a8e94e6665d772fb4ad517321
FinalFantasy0050 <finalfantasy0050@gmail.com>
2025-04-18 00:28:34 -0400

    Update lora.py
    Co-authored-by: legendy4141 <legendy4141@gmail.com>

--- extensions-builtin/Lora/lora.py
+++ extensions-builtin/Lora/lora.py
@@ -1,7 +1,5 @@
 import networks
 
-list_available_loras = networks.list_available_networks
-
 available_loras = networks.available_networks
 available_lora_aliases = networks.available_network_aliases
 available_lora_hash_lookup = networks.available_network_hash_lookup
